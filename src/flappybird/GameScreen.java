611fa29224e88aca98a864e77f20ffbbcc4a8e93
milool <m.olmr@bestfun.cz>
2016-11-02 13:08:50 +0100

    add empty class GameScreen and all the Player logic

---
+++ src/flappybird/GameScreen.java
@@ -0,0 +1,10 @@
+package flappybird;
+
+import javax.swing.JPanel;
+
+public class GameScreen extends JPanel {
+
+	public static final int HEIGHT = 800;
+	public static final int WIDTH = 600;
+	
+}
--- src/flappybird/Player.java
+++ src/flappybird/Player.java
@@ -1,5 +1,68 @@
 package flappybird;
 
-public class Player {
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
 
+public class Player {
+	
+	public static final int WIDTH = 40;
+	public static final int HEIGHT = 40;
+	private static final int SPEED = 1; //koeficient zrychleni
+	private static final int FALL_SPEED = 2; //rychlost padu
+	
+	private BufferedImage img = null;
+	private int x; // x souradnice hrace - nemeni se
+	private int y; // y souradnice hrace - meni se
+	private int speed;
+	
+	public Player(BufferedImage img) {
+		this.img = img;
+		x = (GameScreen.WIDTH - img.getWidth()) / 2;
+		y = GameScreen.HEIGHT / 2;
+		
+		speed = SPEED;
+	}
+	
+	public void reset() {
+		y = GameScreen.HEIGHT / 2;
+		speed = SPEED;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void jump() {
+		speed = -17;
+	}
+	
+	/**
+	 * call this method periodically to make player moving
+	 */
+	public void move() {
+		speed += SPEED;
+		y += speed;
+	}
+	
+	public void paint(Graphics g) {
+		g.drawImage(img, x, y, null);
+	}
+	
+	public int getHeight() {
+		return img.getHeight();
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public Rectangle getSkeleton() {
+		return new Rectangle(x, y, img.getWidth(), img.getHeight());
+	}
+	
 }
