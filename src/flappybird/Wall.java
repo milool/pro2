0541774cf070ae97ee49a830186daf67ed3caf8e
olmrmi1 <olmrmi1@J08-13.uhk.cz>
2016-11-23 13:03:24 +0100

    Update class Wall now complete

--- src/flappybird/Wall.java
+++ src/flappybird/Wall.java
@@ -1,10 +1,10 @@
 package flappybird;
 
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.util.Random;
 
 public class Wall {
-	
-	// vyrobit obrazek pro zed
 	
 	public static final int WIDTH = 45;
 	//rychlost pohybu zdi
@@ -12,7 +12,9 @@ public class Wall {
 	//mezera mezi horni a dolni casti zdi
 	public static final int GAP = 200;
 	
-	//TODO
+	private static int lastWallDistance;
+	
+	private Random random;
 	
 	private static BufferedImage img = null;
 	private int x;
@@ -22,7 +24,53 @@ public class Wall {
 	public Wall(int distanceFromStart) {
 		this.x = distanceFromStart;
 		
-		//TODO
+		random = new Random();
+		generateRandomValuesForWall();
+	}
+
+	private void generateRandomValuesForWall() {
+		y = random.nextInt(GameScreen.HEIGHT-400) + GAP;
+		height = GameScreen.HEIGHT - y;
+	}
+	
+	public void paint(Graphics g) {
+		//dolni zed
+		g.drawImage(img, x, y, null);
+		
+		//horni zed
+		g.drawImage(img, x, y-GameScreen.HEIGHT-GAP, null);
+		
+	}
+	
+	public void move() {
+		x += SPEED;
+		
+		if (x <= -Wall.WIDTH) {
+			x = Wall.lastWallDistance;
+			
+			//TODO
+		}
+		
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public static int getLastWallDistance() {
+		return lastWallDistance;
+	}
+
+	public static void setLastWallDistance(int lastWallDistance) {
+		Wall.lastWallDistance = lastWallDistance;
+	}
+
+	public static void setImg(BufferedImage img) {
+		Wall.img = img;
 	}
 	
 }
