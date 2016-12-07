e0c0fbb2f88efb759050135d7e1560ad3bc017b9
milool <m.olmr@bestfun.cz>
2016-12-07 13:00:56 +0100

    Update main class FlappyBird to use ImageManager

--- src/flappybird/FlappyBird.java
+++ src/flappybird/FlappyBird.java
@@ -1,16 +1,27 @@
 package flappybird;
 
-import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
+
+import flappybird.gameimage.ImageManager;
+import flappybird.gameimage.ImageSourceFile;
 
 public class FlappyBird extends JFrame {
 
-	private GameScreen gs;
+	private ImageManager im;
 	
 	public FlappyBird() {
 		pack();
+	}
+	
+	private void setImageSource() {
+		im = new ImageManager(new ImageSourceFile());
 	}
 	
 	public void initGUI() {
@@ -22,12 +33,54 @@ public class FlappyBird extends JFrame {
 	}
 	
 	public void start() {
-		gs = new GameScreen();
-		gs.init();
-		getContentPane().add(gs, BorderLayout.CENTER);
-		gs.setVisible(true);
-		this.revalidate();
-		this.repaint();
+		setImageSource();
+		
+		class MyThread extends SwingWorker<Object, Object> {
+			private JFrame owner;
+			private JLabel lb;
+			private GameScreen gs;
+			
+			public void setOwner(JFrame owner) {
+				this.owner = owner;
+			}
+			
+			public void doBeforeExecute() {
+				lb = new JLabel("Loading...");
+				lb.setFont(new Font("Arial", Font.BOLD, 30));
+				lb.setForeground(Color.WHITE);
+				lb.setHorizontalAlignment(SwingConstants.CENTER);
+				
+				owner.add(lb);
+				lb.setVisible(true);
+				owner.revalidate();
+				owner.repaint();
+			}
+			
+			@Override
+			protected Object doInBackground() throws Exception {
+				im.prepareImages();
+				gs = new GameScreen(im);
+				gs.init();
+				return null;
+			}
+			
+			@Override
+			protected void done() {
+				super.done();
+				owner.remove(lb);
+				owner.revalidate();
+				owner.add(gs);
+				gs.setVisible(true);
+				owner.revalidate();
+				owner.repaint();
+			}
+
+		}
+		
+		MyThread t = new MyThread();
+		t.setOwner(this);
+		t.doBeforeExecute();
+		t.execute();
 	}
 	
 	public static void main(String[] args) {
